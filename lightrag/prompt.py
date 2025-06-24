5a60dfacc9b163f08d6fda87e8370ac6cc5df69f
Andreas Schiffler <andreas.schiffler@fhws.de>
2025-06-24 15:19:51 +0000

    optimze response prompt

--- lightrag/prompt.py
+++ lightrag/prompt.py
@@ -201,7 +201,6 @@ PROMPTS["rag_response"] = """---Role---
 
 You are a helpful assistant responding to user query about Knowledge Graph and Document Chunks provided in JSON format below.
 
-
 ---Goal---
 
 Generate a concise response based on Knowledge Base and follow Response Rules, considering both the conversation history and the current query. Summarize all information in the provided Knowledge Base, and incorporating general knowledge relevant to the Knowledge Base. Do not include information not provided by Knowledge Base.
@@ -222,12 +221,11 @@ When handling relationships with timestamps:
 
 - Target format and length: {response_type}
 - Use markdown formatting with appropriate section headings
-- Please respond in the same language as the user's question.
 - Ensure the response maintains continuity with the conversation history.
-- List up to 5 most important reference sources at the end under "References" section. Clearly indicating whether each source is from Knowledge Graph (KG) or Document Chunks (DC), and include the file path the creation date if available, in the following format: [KG/DC] file_path [created] created_at
+- List up to 5 most important reference sources at the end under "References" section. Only indicate Document Chunks (DC), and include the file path the creation date if available, in the following format: [<number>](file_path) [created] created_at. Increase the number for each source.
 - If you don't know the answer, just say so.
-- Do not make anything up. Do not include information not provided by the Knowledge Base.
-- Addtional user prompt: {user_prompt}
+- Do not make anything up. Do never include information not provided by the Knowledge Base or Document Chunk.
+- {user_prompt}
 
 Response:"""
 
